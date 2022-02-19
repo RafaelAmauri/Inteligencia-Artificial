10db572ff9a3384910196f4d54b095bed4f3246f
Rafael <amauri.rafael33@gmail.com>
2022-02-19 12:58:40 -0300

    [feat]: Inicio do A*

--- Atividade 1/Pacman/search.py
+++ Atividade 1/Pacman/search.py
@@ -106,13 +106,13 @@ def _depthFirstSearch(problem: SearchProblem, current_pos: tuple, parentNode: tu
     visited_nodes.append(current_pos)
     neighbor_nodes = problem.getSuccessors(current_pos)
     
-    is_goal_found = False
-    solution = ""
-    direction = ""
+    is_goal_found  =  False
+    solution       =  ""
+    direction      =  ""
 
     # Abre uma recursão da função para cada nó que não foi visitado e se o objetivo ainda não foi encontrado.
     for i in neighbor_nodes:
-        if (i[0] not in visited_nodes) and (not is_goal_found):
+        if ( i[0] not in visited_nodes ) and ( not is_goal_found ):
             # Literalmente a direção. Cada i aparece como "((3, 1), 'West', 1)" por exemplo.
             direction = i[1]
 
@@ -132,32 +132,31 @@ def _depthFirstSearch(problem: SearchProblem, current_pos: tuple, parentNode: tu
 def breadthFirstSearch(problem: SearchProblem):
 
     # Sempre começa no nó inicial
-    current_pos    = problem.getStartState()
+    current_pos      =  problem.getStartState()
 
-    visited_nodes  =  []
-    queue          =  util.Queue()
-    
+    visited_nodes    =  []
+    queue            =  util.Queue()
+
     # Esse cara aqui armazena em um dicionário qual nó é o pai de um determinado nó.
     # Por exemplo: parent[4,5] = (5,5) no layout tiny maze.
-    parent         =  {}
+    parent           =  {}
 
     # Armazena quem são os vizinhos de um determinado nó N
-    neighbor_nodes = []
+    neighbor_nodes   =  []
 
-    is_goal_found    =  False
-    
-    neighbor_nodes = problem.getSuccessors(current_pos)
+    is_goal_found    =  False    
+    neighbor_nodes   =  problem.getSuccessors(current_pos)
 
     # Marca o nó inicial como visitado
     visited_nodes.append(current_pos)
 
     # Como o nó inicial não tem pai, o nó pai dele é ele mesmo :P
-    parent[problem.getStartState()] = problem.getStartState()
+    parent[ problem.getStartState() ] = problem.getStartState()
 
     # Populando a queue para simplificar o laço while abaixo
     for i in neighbor_nodes:
         queue.push(i)
-        parent[i[0]] = current_pos
+        parent[ i[0] ] = current_pos
     
     while queue and not is_goal_found:
         current_pos = queue.pop()[0]
@@ -180,15 +179,15 @@ def breadthFirstSearch(problem: SearchProblem):
 
             if not is_in_visited_nodes:
                 queue.push(i)
-                parent[i[0]] = current_pos
+                parent[ i[0] ] = current_pos
     
 
     # Para saber o caminho, o melhor jeito é usar nosso dicionário que armazena o nó pai de outro nó.
     # Essa estratégia é boa no BFS porque pelo funcionamento do algoritmo, navegar pelos nó-pai vai sempre 
     # nos dar a menor distância entre dois nós. No DFS isso já não funcionaria, por exemplo.
-    solution = []
-    child_node  = current_pos
-    parent_node = parent[child_node]
+    solution     =  []
+    child_node   =  current_pos
+    parent_node  =  parent[child_node]
 
     # Aqui eu começo no nó que é o final do labirinto e vou navegando de nó-filho para nó-pai até chegar onde era o ponto inicial. 
     # Assim eu consigo pegar as direções e armazeno elas na variável solution
@@ -200,39 +199,29 @@ def breadthFirstSearch(problem: SearchProblem):
                 parent_node = parent[parent_node]
                 break
 
-
     return solution
 
 
 def uniformCostSearch(problem: SearchProblem):
     
-    # 0 Enquanto is_goal_found for Falso: 
-        # 0.1 Pegar os vizinhos de current_pos
-        # 0.2 Add ele na p_queue
-        # 0.3 Ler a p_queue antes da proxima iteracao e pegar o de maior prioridade
-        # 0.4 Mudar para o de maior prioridade
-        # 0.5 Fazer o hack de mudar de posicao. Nao precisa ser vizinho pra mudar, so setar current_pos = (4,5) por ex
-    # 1 Usar o dict parents para achar o caminho igual no BFS
-    
-    current_pos = problem.getStartState()
-    
+    current_pos    =  problem.getStartState()
     visited_nodes  =  []
 
     # Tem o mesmo propósito que o <parent> do BFS, mas aqui ele armazena um segundo valor: o gasto total para chegar
     # em um nó N a partir do nó inicial
-    parent = {}
+    parent         =  {}
 
     # Items com prioridade maior ficam mais no final! Items com prioridade menos aparecem nas primeiras posições
     # (Priority, Number of Insertion, Item)
-    p_queue = util.PriorityQueue()
+    p_queue        =  util.PriorityQueue()
 
-    is_goal_found = False
+    is_goal_found  =  False
 
     # Marca o nó inicial como visitado
     visited_nodes.append(current_pos)
 
     # Como o nó inicial não tem pai, o nó pai dele é ele mesmo, e obviamente, com custo zero
-    parent[problem.getStartState()] = [problem.getStartState(), 0]
+    parent[ problem.getStartState() ] = [ problem.getStartState(), 0 ]
 
     # Pega os nós vizinhos
     neighbor_nodes = problem.getSuccessors(current_pos)
@@ -258,20 +247,15 @@ def uniformCostSearch(problem: SearchProblem):
                 # push(position, priority)
                 # priority = step_cost do nó atual + custo total do caminho até o nó <position>
                 p_queue.push(position, step_cost + previous_cost)
-                parent[position] = [ current_pos, step_cost + previous_cost ]
-
-            
+                parent[ position ] = [ current_pos, step_cost + previous_cost ]
 
         current_pos = p_queue.pop()
 
-
-    
     # Para saber o caminho, o melhor jeito de novo é usar nosso dicionário que armazena o nó pai de outro nó.
     # Dessa forma, vamos seguir precisamente o caminho que o UCS acima percorreu pra achar o goalState
-    solution = []
-    child_node  = current_pos
-    parent_node = parent[child_node][0]
-    
+    solution     =  []
+    child_node   =  current_pos
+    parent_node  =  parent[child_node][0]    
     
     # Aqui eu começo no nó que é o final do labirinto e vou navegando de nó-filho para nó-pai até chegar onde era o ponto inicial. 
     # Assim eu consigo pegar as direções e armazeno elas na variável solution
@@ -283,7 +267,6 @@ def uniformCostSearch(problem: SearchProblem):
                 parent_node = parent[parent_node][0]
                 break
     
-
     return solution
 
 
@@ -301,6 +284,14 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    # No video do computerphile, isso aqui vai ser literalmente o UCS, so que somando o valor da heuristica ao step_cost
+    # Mas, pra fazer a heuristica, eu tenho que saber a posicao do final :P
+
+    # 0 Achar o goal com o BFS
+    # Copiar e colar o UCS, so que somando a heuristica junto com o step_cost
+    current_pos = problem.getStartState()
+
+
     util.raiseNotDefined()
 
 
