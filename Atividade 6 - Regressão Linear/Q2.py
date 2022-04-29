209dc02f12e988a24434107067fecef268b918e5
Rafael <amauri.rafael33@gmail.com>
2022-04-28 22:14:19 -0300

    Q2 initial commit

---
+++ Atividade 6 - Regressão Linear/Q2.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import LinearRegression
+import sklearn.metrics
+
+df = pd.read_csv("FuelConsumptionCo2.csv")
+
+# FAZER
