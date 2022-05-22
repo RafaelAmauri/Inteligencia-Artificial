419f2b6e32ed02f82c5b3d7ac463cec4932ffc55
Rafael <amauri.rafael33@gmail.com>
2022-05-22 12:59:17 -0300

    [feat] Plot de grafico dos indicadores

--- Trabalho Final/src/predictor.py
+++ Trabalho Final/src/predictor.py
@@ -7,30 +7,37 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 
 class TimeSeriesPredictor:
-    # Modelo para previsões
+    ## Modelo para previsões
     __model: ExponentialSmoothing
 
-    # Lista com codigo dos indicadores que serão avaliados
+    ## Lista com codigo dos indicadores que serão avaliados
     __indicators_codelist: list
 
-    ## Arrays com os conjuntos de treino e teste, bem como as respostas
-    __values_train:    np.empty(0, dtype=np.float64)
-    __values_test:     np.empty(0, dtype=np.float64)
-    __predictions:   np.empty(0, dtype=np.float64)
-    __answers_train: np.empty(0, dtype=np.int32)
-    __answers_test:  np.empty(0, dtype=np.int32)
+    ## Arrays com os conjuntos de treino e teste, bem como as respostas preditas pelo modelo.
+    ## *_data se refere aos valores observados na série. Essas são as variáveis mais importantes
+    ## desse conjunto abaixo. *_years são os anos que foram usados nos períodos de teste e treino,
+    ## e eles são usados principalmente para construir os gráficos com matplotlib
+    __training_data:    np.empty(0, dtype=np.float64)
+    __training_years:   np.empty(0, dtype=np.float64)
+    __testing_data:     np.empty(0, dtype=np.float64)
+    __testing_years:    np.empty(0, dtype=np.float64)
+    __predictions:      np.empty(0, dtype=np.float64)
     
-    # O path absoluto do CSV com os dados
+    ## O path absoluto do CSV com os dados
     __dataset_filepath: str
 
-    # Ano que a serie inicia.
+    ## Ano que a serie inicia.
     __tseries_start_year: int
 
-    # Ano que a serie finaliza.
+    ## Ano que a serie finaliza.
     __tseries_end_year: int
 
-    # Porcentagem de dados para treino de cada indicador
+    ## Porcentagem de dados para treino de cada indicador
     __percentage_train: int
+
+    ## Lista para os nomes por extenso dos indicadores. É usado
+    ## para colocar o título nos gráficos usados no matplotlib
+    __indicators_namelist: dict 
 
 
     '''
@@ -39,16 +46,17 @@ class TimeSeriesPredictor:
     def __init__(self):
         self.__model = ExponentialSmoothing
 
-        self.__data_train    = np.empty(0, dtype=np.float64)
-        self.__data_test     = np.empty(0, dtype=np.float64)
-        self.__predictions   = np.empty(0, dtype=np.float64)
-        self.__answers_train = np.empty(0, dtype=np.int32)
-        self.__answers_test  = np.empty(0, dtype=np.int32)
+        self.__training_data    = np.empty(0, dtype=np.float64)
+        self.__training_years   = np.empty(0, dtype=np.float64)
+        self.__testing_data     = np.empty(0, dtype=np.float64)
+        self.__testing_years    = np.empty(0, dtype=np.float64)
+        self.__predictions      = np.empty(0, dtype=np.float64)
 
-        self.__runtime_metrics = {}
+        self.__runtime_metrics     = {}
+        self.__indicators_namelist = {}
         
 
-    # Gets e Sets :)
+    ## Gets e Sets :)
     def set_tseries_start_year(self, start_year: int):
         self.__tseries_start_year = start_year
 
@@ -79,26 +87,30 @@ class TimeSeriesPredictor:
     def get_dataset_filepath(self):
         return self.__dataset_filepath
 
-    def get_values_train(self):
-        return self.__values_train
+    def get_training_data(self):
+        return self.__training_data
 
-    def get_answers_train(self):
-        return self.__answers_train
+    def get_training_years(self):
+        return self.__training_years
 
-    def get_values_test(self):
-        return self.__values_test
+    def get_testing_data(self):
+        return self.__testing_data
 
-    def get_answers_test(self):
-        return self.__answers_test
+    def get_testing_years(self):
+        return self.__testing_years
 
     def get_predictions(self):
         return self.__predictions
+
+    def get_indicators_namelist(self):
+        return self.__indicators_namelist
 
     def set_runtime_metric(self, metric_name: str, measured_time: float):
         self.__runtime_metrics[metric_name] = f"{measured_time:.6f} s"
     
     def get_runtime_metrics(self):
         return self.__runtime_metrics
+
 
 
     '''
@@ -112,10 +124,14 @@ class TimeSeriesPredictor:
         ## Início to cronômetro para separação
         start = time.perf_counter()
 
+        ## Abrindo o CSV
         df = pd.read_csv(self.get_dataset_filepath())
 
+        ## Pega a coluna do dataset que tem as informações desse indicador
         df_row_indicator = df.loc[df['Indicator Code'] == indicator_code]
-        indicator_name = df_row_indicator['Indicator Name']
+
+        ## Armazena o nome do indicador em uma lista para evitar ter que abrir várias vezes o CSV
+        self.__indicators_namelist[indicator_code] = df_row_indicator['Indicator Name']
 
         ## Indica qual o ultimo ano para treinamento de acordo com a porcentagem de treino
         last_year_training = int((self.get_tseries_end_year() - self.get_tseries_start_year())*self.get_percentage_train() + self.get_tseries_start_year())
@@ -127,135 +143,64 @@ class TimeSeriesPredictor:
         # Armazenando os valores 
 
         ## !! Trocar os nomes, porque endog da funcao so recebe uma serie, não um par <value> e <predicted> !!
-        self.__values_train  = np.asarray(tmp.keys())
-        self.__answers_train = np.asarray(tmp.values)
+        self.__training_data   = np.concatenate(tmp.values, axis=0)
+        self.__training_years  = np.asarray(tmp.keys())
 
-        tmp = df_row_indicator.loc[:, f"{last_year_training+1}":f"{self.get_tseries_end_year()}"]
-        self.__values_test  = np.asarray(tmp.keys())
-        self.__answers_test = np.asarray(tmp.values)
+        tmp = df_row_indicator.loc[:, f"{last_year_training}":f"{self.get_tseries_end_year()}"]
 
-        print(self.__values_train)
-        print(self.__values_test)
+        self.__testing_data   = np.concatenate(tmp.values, axis=0)
+        self.__testing_years  = np.asarray(tmp.keys())
+
+        end   = time.perf_counter()
+        self.set_runtime_metric("Separação dos dados de teste e treino", end - start)
         
 
     '''
-    Treina o modelo com as imagens em self.get_images_train(). Deve ser chamada
+    Treina o modelo com os dados em self.get_training_data(). Deve ser chamada
     somente depois de self.split_train_test()
     '''
     def train_model(self):
         start = time.perf_counter()
-        self.__model.fit(self.get_images_train(), self.get_answers_train())
+
+        self.__model = ExponentialSmoothing(
+                                            endog=self.get_training_data(),
+                                            initialization_method='estimated',
+                                            trend='add'
+                                            ).fit()
+
         end   = time.perf_counter()
 
         self.set_runtime_metric("Etapa de treinamento do modelo", end-start)
 
 
     '''
-    Faz a previsão das classes BIRADS das imagens de teste. Essa função deve ser chamada 
+    Faz a previsão dos dados no conjunto de teste. Essa função deve ser chamada 
     somente depois de self.split_train_test() e de self.train_model()
     '''
-    def predict_test_images(self):
+    def predict_testing_data(self):
         start = time.perf_counter()
-        self.__predictions = self.__model.predict(self.get_images_test())
+        self.__predictions = self.__model.forecast(steps=len(self.get_testing_data()))
         end   = time.perf_counter()
 
-        self.set_runtime_metric("Identificação das imagens de teste", end - start)
+        self.set_runtime_metric("Predição dos próximos valores na série", end - start)
 
 
-'''
-import pandas as pd
-
-import matplotlib.pyplot as plt
-
-from statsmodels.tsa.holtwinters import ExponentialSmoothing, SimpleExpSmoothing
-
-
-class TimeSeriesPredictor():
-
-    # O preditor
-    __predictor: ExponentialSmoothing
-
-    # O CSV com os dados
-    __dataset_file: str
-
-    # Lista com codigo dos indicadores que serão avaliados
-    __indicators_codelist: list
-
-    # Ano que o treinamento inicia. Deve ser maior que 1960
-    __start_date_train:int
-
-    # Ano que o treinamento finaliza. Deve ser menor que 2014
-    __end_date_train:int
-
-
-    def __init__(self, dataset_file, indicators_codelist, start_date, end_date):
-        self.__dataset_file = dataset_file
-        self.__indicators_codelist = indicators_codelist
-
-        self.__start_date_train = start_date
-        self.__end_date_train   = end_date
-
-
-    
     def plot_indicators(self):
-        df = pd.read_csv(self.__dataset_file)
+        start = time.perf_counter()
 
-        for indicator_code in self.__indicators_codelist:
+        for indicator_code in self.get_indicators_codelist():
+            self.split_train_test(indicator_code)
+            self.train_model()
+            self.predict_testing_data()
 
-            # Get row of indicator
-            df_indicator = df.loc[df['Indicator Code'] == indicator_code]
-            indicator_name = df_indicator['Indicator Name']
-
-            anos = []
-            valores = []
-            
-            # Adicionar valores para aquele indicador entre start_date e end_date
-            for i in range(self.__start_date_train, self.__end_date_train):
-                anos.append(i)
-                valores.append(df_indicator[f"{i}"].values[0])
-
-            
-            plt.title(f"{indicator_name.values[0]}")
-            plt.plot(anos, valores)
+            plt.title(f"{self.get_indicators_namelist()[indicator_code]}")
+            plt.plot(self.get_training_years(), self.get_training_data(), c="green", label="Treino")
+            plt.plot(self.get_testing_years(), self.get_testing_data(), c="blue", label="Teste")
+            plt.plot(self.get_testing_years(), self.get_predictions(), c="red", label="Previsão")
+            plt.legend()
             plt.show()
 
             plt.clf()
 
-
-    def predict_indicators(self):
-        df = pd.read_csv(self.__dataset_file)
-
-        for indicator_code in self.__indicators_codelist:
-
-            # Get row of indicator
-            df_indicator = df.loc[df['Indicator Code'] == indicator_code]
-            indicator_name = df_indicator['Indicator Name']
-
-            anos = []
-            valores = []
-            
-            # Adicionar valores para aquele indicador entre start_date e end_date
-            for i in range(self.__start_date_train, self.__end_date_train):
-                anos.append(i)
-                valores.append(df_indicator[f"{i}"].values)
-        
-            print(f"VALORES: {valores}\n\n")
-
-            self.__predictor = ExponentialSmoothing(endog=valores, trend='mul', initialization_method="heuristic").fit().forecast(steps=10)
-            
-            print(self.__predictor)
-
-            outros = []
-            outros_2 = []
-            for i in range(self.__end_date_train, 2014):
-                outros.append(i)
-                outros_2.append(df_indicator[f"{i}"].values)
-
-            plt.plot(anos, valores, c="b")
-            plt.plot(outros,outros_2, c="green")
-            plt.plot(outros,self.__predictor, c="r")
-            plt.show()
-
-            plt.clf()
-
-'''
+        end   = time.perf_counter()
+        self.set_runtime_metric("Geração dos gráficos dos indicadores", end - start)
--- Trabalho Final/src/predictor_test.py
+++ Trabalho Final/src/predictor_test.py
@@ -4,8 +4,8 @@ pred = predictor.TimeSeriesPredictor()
 
 pred.set_dataset_filepath("/home/rafael/PUC/PUC-Minas/Inteligencia-Artificial/Trabalho Final/assets/BR.csv")
 pred.set_indicators_codelist(['SP.POP.TOTL', 'SP.RUR.TOTL'])
-pred.set_percentage_train(75)
+pred.set_percentage_train(90)
 pred.set_tseries_start_year(1960)
 pred.set_tseries_end_year(2014)
 
-pred.split_train_test('SP.POP.TOTL')
+pred.plot_indicators()
