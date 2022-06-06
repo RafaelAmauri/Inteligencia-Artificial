39e46b733e4b165e94df01d59c39bf545336d489
Rafael <amauri.rafael33@gmail.com>
2022-06-06 20:16:58 -0300

    feat: add main.py and readme

--- Trabalho Final/src/main.py
+++ Trabalho Final/src/main.py
@@ -10,8 +10,13 @@ pred.percentage_validation = 2
 pred.tseries_start_year = 1960
 pred.tseries_end_year = 2020
 
+
+
+pred.plot_indicators()
+
 '''
-Testes de validação para ajustar os parâmetros do modelo!!
+## Exemplo que printa os valores para as métricas de avaliação no conjunto de validação.
+## Esse foi o código usado para ajustar os parâmetros do modelo.
 
 for indicator_code in pred.indicators_codelist:
     pred.split_train_test_val(indicator_code)
@@ -24,5 +29,3 @@ for indicator_code in pred.indicators_codelist:
     print(f"Métricas de avaliação para a previsão de '{indicator_code}'")
     pred.predict_validation_data()
 '''
-
-pred.plot_indicators()
